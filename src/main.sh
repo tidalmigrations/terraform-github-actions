0967bae5c644873d88269cda0a8b5ba7e8f13137
Prem Rara <p@rara.dev>
2019-12-23 19:51:08 -0500

    terrar gunts?

--- src/main.sh
+++ src/main.sh
@@ -138,7 +138,7 @@ function main {
   source ${scriptDir}/terraform_output.sh
 
   parseInputs
-  parseTerrarguntInputs
+  parseTerraguntInputs
   configureCLICredentials
   cd ${GITHUB_WORKSPACE}/${tfWorkingDir}
 
